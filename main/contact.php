139e3ad57d7cbd02c3d00d4a8482ac53906ef1e8
Miep <91285462+Miep1194390@users.noreply.github.com>
2022-05-13 16:14:48 +0200

    contact (#25)
    * php toegevoegt css toegevoegt
    
    * pagina's toegevoegt + standaart header toegevoegt (niet definitief)
    
    * Dashboard Login+Registreren
    
    * admin page toegevoegd
    
    * begonnen aan over ons page
    
    * new pr
    
    * NEW PR TEST
    
    * dashboard fixes
    
    * dashboard bugs
    
    * kan inloggen via elke page
    
    * Boeken, calender fixed, vluchten aangepast, header dashboard url aangepast
    
    * x
    
    * inloggen verandert (html del)
    
    * boekenredirect extra elseif
    
    * vluchten changes
    
    * vluchten changes front
    
    * link-icon, delete mijnaccount
    
    * vluchten en contact verandert
    
    * javascript accordion, contact changes, vluchten
    
    * vluchten
    
    * contact
    
    Co-authored-by: ililuab <1194420@student.roc-nijmegen.nl>
    Co-authored-by: ililuab <94444127+ililuab@users.noreply.github.com>

--- main/contact.php
+++ main/contact.php
@@ -8,50 +8,86 @@
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="../media/BookAndGoLogo.jpg" type="image/gif" sizes="16x16">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <title>Book And Go</title>
 </head>
 
 <body>
     <main>
-        <div class="header-img" src="../media/oldbackground.jpg" alt="Snow" style="width:100%;">
-            <div class="slideshow-container">
-                <div class="mySlides fade">
-                    <img src="../media/vacationpicture1.jpg" style="width:100%">
+        <div class="header_vlucht_resultaten">
+            <div class="headertext_vlucht_resultatenOuter">
+                <div class="headertext_vlucht_resultaten">
+                    <a class="image-vluchtenOuter" href="index.php">
+                        <img class="image-vluchten" src="../media/BookAndGoLogo.jpg" alt="Book and go Logo">
+                    </a>
                 </div>
-
-                <div class="mySlides fade">
-                    <img src="../media/vacationpicture2.jpg" style="width:100%">
-                </div>
-
-                <div class="mySlides fade">
-                    <img src="../media/vacationpicture3.jpg" style="width:100%">
-                </div>
-            </div>
-            <div style="text-align:center">
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
             </div>
         </div>
-        <br>
-        <div class="top-center">
 
+        <div class="spacer2"></div>
+        <div class="spacer2"></div>
+        <div class="spacer2"></div>
 
-            <div class="header">
+        <div class="contactmainbackground">
+            <div class="contactmainbackgroundinner1"><img class="image-contact" src="../media/BookAndGoLogo.jpg" alt=""></div>
+            <div class="contactmainbackgroundinner2">
+                <div class='contact-page-text'>Meer weten over boekingen, of ben je journalist en heb je in de media vragen over BookAndGo? Neem dan contact met ons op via de links hier.</div>
+            </div>
+            <div class="contactmainbackgroundinner3"></div>
+        </div>
+        <div class="spacer"></div>
+        <div class="contactimagebg"></div>
+        <div class="contactunderlinks">
+            <div class="contactunderlinksInner">
+                <button class="accordion">
+                    <div class="contacttextouter">
+                        <div class="contacttextunder">Klantenservice <i class="arrow down"></i></div>
+                    </div>
+                </button>
+                <div class="panel">
+                    <p class='p-contact'>Uw antwoord niet online gevonden? Vind onze klantenservice gegevens via de link hieronder.</p><a class='p-contact' href="klantenservice.php"><i class="arrow right"></i> Klantenservice</a>
+                </div>
 
-                <div class="dropdown">
-                    <a href="../main/account.php" class="dropbtn">Inloggen</a>
-                    <div class="dropdown-content">
-                        <a href="../main/dashboard.php">Dashboard</a>
+                <button class="accordion">
+                    <div class="contacttextouter">
+                        <div class="contacttextunder">Algemene voorwaarden <i class="arrow down"></i></div>
+                    </div>
+                </button>
+                <div class="panel">
+                    <p class='p-contact'>Deze Algemene Vervoersvoorwaarden zijn van toepassing op alle vluchten, of vluchtsegmenten,
+                        waarbij de BookandGo Code is aangegeven in het vak Carrier (Vervoerder) van uw ticket of van
+                        de overeenkomstige coupon, alsmede de andere situaties zoals gespecificeerd in de Algemene
+                        Vervoersvoorwaarden.
+                    </p>
+                    <a class='p-contact' href="../media/BookAndGo_Algemene_voorwaarden.pdf" download><i class="arrow right"></i> Algemene voorwaarden</a>
+                </div>
+
+                <button class="accordion">
+                    <div class="contacttextouter">
+                        <div class="contacttextunder">Privacy Policy <i class="arrow down"></i></div>
+                    </div>
+                </button>
+                <div class="panel">
+                    <p class='p-contact'>In dit privacybeleid geven we meer informatie over de persoonsgegevens die we verzamelen en gebruiken en wat uw rechten zijn. Klik op de betreffende paragraaf hieronder voor meer informatie.</p>
+                    <a class='p-contact' href="../media/BookAndGo_Privacy_Policy.pdf" download><i class="arrow right"></i> Privacy Policy</a>
+                </div>
+                <div class="underheader-vluchten">
+                    <div class="underheaderInner-vluchten">
+                        <a class="headerlinks-vluchten" href="locaties.php">Locaties</a>
+                        <a class="headerlinks-vluchten" href="index.php">Boeken</a>
+                        <a class="headerlinks-vluchten" href="klantenervice.php">Klantenservice</a>
+                        <a class="headerlinks-vluchten" href="over_ons.php">Privacy policy</a>
                     </div>
                 </div>
-                <a class="Header-links" href="../main/locaties.php">Locaties</a>
-                <a href="index.php"><img class="Header-Logo" src="../media/BookAndGoLogo.jpg" alt="Logo bookandgo"></a>
-                <a class="Header-links" href="../main/over_ons.php">Over ons</a>
-                <a class="Header-links" href="../main/contact.php">Contact</a>
+
+
+
             </div>
+        </div>
     </main>
-    <script src="../js/main.js"></script>
+
+</body>
+<script src="../js/accordion.js"></script>
 
 </html>
