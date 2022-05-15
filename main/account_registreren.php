c4578dd5ad7d1d1610630ceaaa6870879d0f60d3
Miep <91285462+Miep1194390@users.noreply.github.com>
2022-05-15 23:09:03 +0200

    account.php in progress (#29)
    * php toegevoegt css toegevoegt
    
    * pagina's toegevoegt + standaart header toegevoegt (niet definitief)
    
    * Dashboard Login+Registreren
    
    * admin page toegevoegd
    
    * begonnen aan over ons page
    
    * new pr
    
    * NEW PR TEST
    
    * dashboard fixes
    
    * dashboard bugs
    
    * kan inloggen via elke page
    
    * Boeken, calender fixed, vluchten aangepast, header dashboard url aangepast
    
    * x
    
    * inloggen verandert (html del)
    
    * boekenredirect extra elseif
    
    * vluchten changes
    
    * vluchten changes front
    
    * link-icon, delete mijnaccount
    
    * vluchten en contact verandert
    
    * javascript accordion, contact changes, vluchten
    
    * vluchten
    
    * contact
    
    * contact, vuchten .php changes
    
    * contact, index, klantenservices, overons ,vluchten changes
    
    * contact locaties verandert
    
    * css update
    
    * update
    
    * login.php in progress
    
    Co-authored-by: ililuab <1194420@student.roc-nijmegen.nl>
    Co-authored-by: ililuab <94444127+ililuab@users.noreply.github.com>

---
+++ main/account_registreren.php
@@ -0,0 +1,96 @@
+<?php
+include_once('../includes/connect.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="../media/BookAndGoLogo.jpg" type="image/gif" sizes="16x16">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Account</title>
+</head>
+
+<body>
+    <main>
+        <div class="header_vlucht_resultaten">
+            <div class="headertext_vlucht_resultatenOuter">
+                <div class="headertext_vlucht_resultaten">
+                    <div class="dropdown">
+                        <div class="Header-links">Beheren</div>
+                        <div class="dropdown-content">
+                            <div><a href="index.php">Vlucht boeken</a></div>
+                            <div><a href="dashboard.php">Vlucht wijzigen</a></div>
+                            <div><a href="dashboard.php">Vlucht annuleren</a></div>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <div class="Header-links">Service</div>
+                        <div class="dropdown-content">
+                            <div><a href="klantenservice.php">Klantenservice</a></div>
+                            <div><a href="contact.php">Contact</a></div>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <div class="Header-links">Account</div>
+                        <div class="dropdown-content">
+                            <div><a href="dashboard.php">Dashboard</a></div>
+                            <div><a href="account.php">Inloggen</a></div>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <div class="Header-links">Over</div>
+                        <div class="dropdown-content">
+                            <div><a href="locaties.php">Locaties</a></div>
+                            <div><a href="over_ons.php">Over ons</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="spacer2"></div>
+        <div class="account-bg">
+            <div class="account-bg-inner">
+                <div class="account-img"><img class='account-img-inner' src="../media/loginimage.png" alt="Slot"></div>
+                <div class="account-login">
+                    <div class="account-login-inner">LOGIN</div>
+                </div>
+                <!-- REGISTERFORM -->
+                <div class='account-registreren-form'>
+                    <form action="registreren.php" method="post">
+                        <h1>Registeren</h1>
+                        <div>
+                            <input type="text" class="" name="name" placeholder="Naam" />
+                        </div>
+                        <div>
+                            <input type="text" class="" name="username" placeholder="Gebruikersnaam" />
+                        </div>
+                        <div>
+                            <input type="password" class="" name="password" placeholder="Wachtwoord" />
+                        </div>
+                        <div>
+                            <button name="registeren">Registeren</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+
+
+    </main>
+
+</body>
+
+</html>
