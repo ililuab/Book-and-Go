b4e744b42e28910f97acc39629eb21fdd7597012
ililuab <1194420@student.roc-nijmegen.nl>
2022-05-16 16:02:23 +0200

    kaas

--- main/over_ons.php
+++ main/over_ons.php
@@ -19,7 +19,7 @@
         <div class="header_vlucht_resultaten">
                 <div class="headertext_vlucht_resultatenOuter">
                     <div class="headertext_vlucht_resultaten">
-                        <img class="header_logo" src="../media/BookAndGoLogo.jpg" alt="BookAndGoLogo">
+                    <a href="index.php" ><img class="header_logo" src="../media/BookAndGoLogo.jpg" alt="BookAndGoLogo"></a>
                         <div class="header_logo_text">Book and Go</div>
                     <div class="dropdown">
                             <div class="Header-links">Beheren</div>
