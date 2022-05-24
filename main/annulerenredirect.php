19e69664d0f2df50c86bfe24c6271f54cbc18918
Miep1194390 <1194390@student.roc-nijmegen.nl>
2022-05-24 12:25:50 +0200

    delete vluchten

---
+++ main/annulerenredirect.php
@@ -0,0 +1,28 @@
+<?php
+include_once('../includes/connect.php');
+if(isset($_POST['annuleren']))
+{
+    $annuleren = $_POST['annuleren'];
+
+    try {
+
+        $query = "DELETE FROM boekingen WHERE vluchtId=? LIMIT 1";
+        $statement = $conn->prepare($query);
+        $statement->bindParam(1, $annuleren, PDO::PARAM_INT);
+        $query_execute = $statement->execute();
+
+        if($query_execute)
+        {
+            echo 'hier1';
+        }
+        else
+        {
+            echo 'hier2';
+        }
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+?>
--- main/vluchteninzien.php
+++ main/vluchteninzien.php
@@ -30,14 +30,13 @@ if (isset($_SESSION['sess_user_id']) && $_SESSION['sess_user_id'] != "") {
 <body>
     <main class='main-dashboard'>
         <div class="sidebar">
-            <header>BookAndGo</header>
-            <header><?php echo 'Ingelogd als: ' . $_SESSION['sess_user_name'] ?></header>
+            <header class='headerdb'><?php echo 'Ingelogd als: ' . $_SESSION['sess_user_name'] ?></header>
             <ul>
                 <li>
-                    <a href="index.php"><i class="fa-solid fa-house-user"></i>Book and Go</a>
+                <a href="index.php"><i class="fa-solid fa-house-user"></i>Book and Go</a>
                     <a href="dashboard.php"><i class="fa-solid fa-house-user"></i>Dashboard</a>
                     <a href="vluchteninzien.php"><i class="fa-solid fa-eye"></i>Vluchten Inzien</a>
-                    <a href="#"><i class="fa-solid fa-ban"></i>Vluchten Annuleren</a>
+                    <a href="vluchtenannuleren.php"><i class="fa-solid fa-ban"></i>Vluchten Annuleren</a>
                     <a href="uitloggen.php"><i class="fa-solid fa-arrow-right-from-bracket"></i>Uitloggen</a>
                 </li>
             </ul>
