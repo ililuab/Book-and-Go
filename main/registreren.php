f9f38dfd7c3957e5aa3c7aeb47717b2c08e92327
Miep <1194390@student.roc-nijmegen.nl>
2022-06-08 18:05:10 +0200

    bugfixes, dashboard

--- main/registreren.php
+++ main/registreren.php
@@ -2,25 +2,33 @@
 include_once("../includes/connect.php");
 if (isset($_POST['registreren'])) {
     if ($_POST['email'] != "" || $_POST['name'] != "" || $_POST['username'] != "" || $_POST['password'] != "") {
-        $username = $_POST['username'];
-        
-        
-        $sql = "INSERT INTO users (username, email, password, name) VALUES (:username, :email, :password, :name)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':username', $_POST['username']);
-        $stmt->bindParam(':email', $_POST['email']);
-        $stmt->bindParam(':password', $_POST['password']);
-        $stmt->bindParam(':name', $_POST['name']);
-        $stmt->execute();
 
-        echo "
-        <script>alert('Account aangemaakt!')</script>
-        <script>window.location = 'account_registreren.php'</script>
-    ";
+        if (isset($_POST['email'])) {
+            $email = $_POST['email'];
+            $sql = "SELECT email FROM users WHERE email = $email";
+            $prepare = $conn->prepare($sql);
+            $rowCount = $prepare->rowCount();
+
+            if ($rowCount > 0 || $_SESSION['sess_name'] == "adminaccountje") {
+                $sql = "INSERT INTO users (username, email, password, name) VALUES (:username, :email, :password, :name)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':username', $_POST['username']);
+                $stmt->bindParam(':email', $_POST['email']);
+                $stmt->bindParam(':password', $_POST['password']);
+                $stmt->bindParam(':name', $_POST['name']);
+                $stmt->execute();
+        
+                echo "<script>alert('Account aangemaakt!')</script>
+                      <script>window.location = 'account_registreren.php'</script>";
+            }
+            else {
+                echo "<script>alert('Account bestaat al!')</script>
+                      <script>window.location = 'account_registreren.php'</script>";
+            }
+        }
+          
     } else {
-        echo "
-                        <script>alert('Alles invullen!')</script>
-                        <script>window.location = 'account_registreren.php'</script>
-                    ";
+        echo "<script>alert('Alles invullen!')</script>
+              <script>window.location = 'account_registreren.php'</script>";
     }
 }
